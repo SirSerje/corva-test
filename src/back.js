5b4d2aa87f523bc75066a48d45e70aa2c9a28c4b
SirSerje <sir.serje@gmail.com>
2019-05-06 14:05:24 +0300

    initial commit

---
+++ src/back.js
@@ -0,0 +1,34 @@
+var back = require('express')();
+var http = require('http').Server(back);
+var io = require('socket.io')(http);
+var randomNumber = require('./randomNumber');
+
+var port = process.env.PORT || 3000;
+
+back.get('/', function (req, res) {
+  res.send('Use socket.io-client to connect to the server...');
+});
+
+io.on('connection', function (socket) {
+  console.log('connect');
+
+  var unsubscribe = randomNumber.subscribe(function (number) {
+    console.log(number);
+
+    var data = {
+      value: number,
+      timestamp: Number(new Date()),
+    };
+
+    socket.emit('data', data);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('disconnect')
+    unsubscribe();
+  });
+});
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
+});
